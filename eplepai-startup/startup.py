7f8ca4c1209fc1ac4b0f84c9f2f611d87cfdd10f
ankelefs <anker@lefstad.net>
2022-02-24 11:51:45 +0100

    Update startup.py

--- eplepai-startup/startup.py
+++ eplepai-startup/startup.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
+import time
 import os
 
 os.system("cd /home/pi/Documents/elsys-prosjekt")
+time.sleep(10) # Vil teste om Raspberry Pi-en klarer en pull-request fra GitHub etter 10 s
 os.system("git pull")
 
