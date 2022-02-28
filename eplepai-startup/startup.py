ad24fd77caf117d59fe4032f982e0546c362b85a
ankelefs <anker@lefstad.net>
2022-02-28 12:09:15 +0100

    FIKSET?

--- eplepai-startup/startup.py
+++ eplepai-startup/startup.py
@@ -3,8 +3,7 @@ import time
 import os
 
 time.sleep(15) # Buffer for å sikre internett-tilgang FØR "git pull"
-os.system('cd /home/pi/Documents/elsys-prosjekt')
-os.system('git pull')
+os.system('cd /home/pi/Documents/elsys-prosjekt && git pull')
 
-time.sleep(10) # Buffer før datainnsamling
+# time.sleep(10) # Buffer før datainnsamling
 # os.system("python3 *adresse til program*")
