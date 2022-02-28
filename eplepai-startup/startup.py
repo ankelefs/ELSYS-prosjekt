9f1b0eaa6adebeffd422daf9f2845c312bd7dda7
Rikke Nielsen <RikkeNielsen@dhcp-10-24-36-207.wlan.ntnu.no>
2022-02-28 09:20:46 +0100

    Anker sin endring

--- eplepai-startup/startup.py
+++ eplepai-startup/startup.py
@@ -6,8 +6,4 @@ os.system("cd /home/pi/Documents/elsys-prosjekt")
 time.sleep(10) # Vil teste om Raspberry Pi-en klarer en pull-request fra GitHub etter 10 s
 os.system("git pull")
 
-<<<<<<< HEAD
-print("hei")
-=======
 # KAN VÆRE AT DENNE FILEN MÅ LAGRES LOKALT PÅ EPLEPAIEN
->>>>>>> 4dccd0a5b154454fb4846c7b694c01d36153bf5b
