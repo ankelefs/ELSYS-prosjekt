4dccd0a5b154454fb4846c7b694c01d36153bf5b
ankelefs <anker@lefstad.net>
2022-02-28 09:15:32 +0100

    kommentar

--- eplepai-startup/startup.py
+++ eplepai-startup/startup.py
@@ -6,3 +6,4 @@ os.system("cd /home/pi/Documents/elsys-prosjekt")
 time.sleep(10) # Vil teste om Raspberry Pi-en klarer en pull-request fra GitHub etter 10 s
 os.system("git pull")
 
+# KAN VÆRE AT DENNE FILEN MÅ LAGRES LOKALT PÅ EPLEPAIEN
