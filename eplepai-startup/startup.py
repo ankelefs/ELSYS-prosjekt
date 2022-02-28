5bf6155ff1da8a21f443a03b3226b7886419d23f
ankelefs <anker@lefstad.net>
2022-02-28 11:37:13 +0100

    Test-endring

--- eplepai-startup/startup.py
+++ eplepai-startup/startup.py
@@ -3,8 +3,8 @@ import time
 import os
 
 time.sleep(15) # Buffer for å sikre internett-tilgang FØR "git pull"
-os.system("cd /home/pi/Documents/elsys-prosjekt")
-os.system("git pull")
+os.system('cd /home/pi/Documents/elsys-prosjekt')
+os.system('git pull')
 
 time.sleep(10) # Buffer før datainnsamling
 # os.system("python3 *adresse til program*")
