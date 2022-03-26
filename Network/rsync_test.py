983525184e3942a98eea24739e7a03bba27bd294
eplepai1302 <74707357+eplepai1302@users.noreply.github.com>
2022-03-26 15:06:38 +0100

    rsync

---
+++ Network/rsync_test.py
@@ -0,0 +1,8 @@
+import time
+import os
+
+
+#Må finne måte å kjøre denne som bakgrunnsprogramm
+while(1):
+    os.system("rsync -b *også må jeg finne ut hostname og hvor den skal")
+    time.sleep(3600*2)
