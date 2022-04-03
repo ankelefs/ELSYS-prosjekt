4e43dba1b9e9f251951fef0ae4732b203cd191ff
Maria Reierskog Bolme <mariabolme@dhcp-10-22-78-220.wlan.ntnu.no>
2022-04-03 15:25:18 +0200

    nei

--- Nettside/webkurs/elsysapp/gitpull_nettside.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-import time
-import os
-
-time.sleep(15) # Buffer for å sikre internett-tilgang FØR "git pull"
-os.system('cd /Desktop/Elsys/elsys-prosjekt/Nettside/Webkurs && git pull') # Må ha '&&' for å kjøre 'git pull' i samme directory
-
-# time.sleep(10) # Buffer før datainnsamling
-# os.system("python3 *adresse til program*")
