361a3094fa033fed13a4bb9330d2de4d8ba29c6b
ankelefs <anker@lefstad.net>
2022-04-04 09:23:51 +0200

    Nytt program

---
+++ pull_and_analyze.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+import time
+import os
+
+sleep = 10
+sixHours = 3600*6
+elsysProsjektMappe = ''
+
+while True:
+    time.sleep(sleep)
+    try:
+        os.system(f'cd {elsysProsjektMappe} && git pull') # Henter nye lydopptak
+        print('git push success')
+    except: 
+        print('git push unsuccesful')
+
+    time.sleep(sleep)
+    try:
+        os.system(f'cd {elsysProsjektMappe} && python3 raspi_analyze.py') # Analyse
+        print('Analysis complete')
+    except:
+        print('Analysis unsuccessfull')
+        
+    # Kun for optimalisering av lagring
+    try:
+        os.system(f'cd {elsysProsjektMappe} && rm *.bin') # Fjerner alle binær-filer
+        print('Deletion success')
+    except: 
+        print('Deletion failed')
+        
+    try:
+        os.system(f'cd {elsysProsjektMappe} && git add . && git commit -m "Fjerning av gamle lydopptak" && git push') # Fjerner alle binær-filer fra git
+        print('Deletion on git success')
+    except: 
+        print('Deletion on git failed')
+        
+    time.sleep(sixHours)
