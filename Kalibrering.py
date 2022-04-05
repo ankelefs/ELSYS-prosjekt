cbeb53216dfd1fadedce997308f6fc256fb0d2b1
Andrrob <andrrob@stud.ntnu.no>
2022-04-05 16:02:12 +0200

    ny_kalib

--- Kalibrering.py
+++ Kalibrering.py
@@ -12,3 +12,17 @@ def finn_kalibrering(kalib_fil, målt_verdi):
 
     diff = np.argmax(spectrum) - målt_verdi
     return diff
+
+
+
+def finn_v0(kalib_fil, målt_verdi_Leq):
+    sample_period, data = raspi_analyze.raspi_import(kalib_fil)
+    num_of_samples = data.shape[0]
+    T = num_of_samples  * sample_period
+    sum_V = 0
+    for i in range(0, len(data)):
+        sum_V += (data[i])**2
+
+    v0 = np.sqrt(1/T * 1/(10**(målt_verdi_Leq/20)) * sum_V)
+    return v0
+
