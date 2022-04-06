a3c520dae9030b4311935eb1050900294ae8fbd0
Andrrob <andrrob@stud.ntnu.no>
2022-04-06 11:27:03 +0200

    test

--- test_plott.py
+++ test_plott.py
@@ -25,17 +25,17 @@ def raspi_import(path, channels=1):
         data = data.reshape((-1, channels))
     return sample_period, data
 
-sample_period, data = raspi_import('Y2022-M04-D04-H10-M38-S12.bin')
+#sample_period, data = raspi_import('Y2022-M04-D04-H10-M38-S12.bin')
 
-sample_period *= 1e-6  # change unit to micro seconds
+#sample_period *= 1e-6  # change unit to micro seconds
 
 # Generate time axis
-num_of_samples = data.shape[0]  # returns shape of matrix
-t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
+#num_of_samples = data.shape[0]  # returns shape of matrix
+#t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 
 # Generate frequency axis and take FFT
-freq = np.fft.rfftfreq(n=num_of_samples, d=sample_period)
-spectrum = np.fft.rfft(data, axis=0)  # takes FFT of all channels
+#freq = np.fft.rfftfreq(n=num_of_samples, d=sample_period)
+#spectrum = np.fft.rfft(data, axis=0)  # takes FFT of all channels
 
 #dBA_list = []
 dBA_dict = {6.3: -85.4, 8: -77.6, 10: -70.4, 12.5: -63.6, 16: -56.4, 20: -50.4, 25: -44.8, 31.5: -39.5, 40: -34.5, 50: -30.3, 63: -26.2, 80: -22.4, 100: -19.1, 125: -16.2, 160: -13.2, 200: -10.8, 250: -8.7, 315: -6.6, 400: -4.8, 500: -3.2, 630: -1.9, 800: -0.8, 1000: 0.0, 1250: 0.6, 1600: 1.0, 2000: 1.2, 2500: 1.3, 3150: 1.2, 4000: 1.0, 5000: 0.6, 6300: -0.1, 8000: -1.1, 10000: -2.5, 12500: -4.3, 16000: -6.7, 20000: -9.3} #inneholder tabellen over generelle dBA verdier
@@ -123,32 +123,59 @@ def todB_num(verdi_rfft):
 
 
 
-def ekvivalentnivå_mv0(måling_data, tid_fil, v0):
+def ekvivalentnivå_mv0(måling_data, v0):
     sum = 0
     for i in range(0, len(måling_data)):
-        sum += (data[i]/v0)**2
-    L = 20*np.log(1/tid_fil + sum)
+        sum += (måling_data[i]/v0)**2
+    L = 10*np.log(1/len(måling_data) + sum)
+    return float(L)
 
+#Antar
+v0 = 0.02
 
 antall_filer = 2
-liste_filer = ['Y2022-M04-D04-H10-M38-S12.bin', 'Y2022-M04-D04-H10-M38-S17.bin']
-'''
+liste_filer = ['Lydfiler/lastebil.bin', 'Lydfiler/bil.bin']
 def plott(antall_filer, liste_filer):
+    tid = 0
+    tid_vec = []
     y_db = 0
     L = 0
+    L_eq = []
+    dBA_num = 0
     for n in range(0, antall_filer):
+        print(liste_filer[n])
         sample_period, data = raspi_import(liste_filer[n])
+        num_of_samples = data.shape[0]
         freq = np.fft.rfftfreq(n=num_of_samples, d=sample_period)
         spectrum = np.fft.rfft(data, axis=0) 
-        yy, yh = butter_bandpass_filter(x, 200, 800, 31250, order=8)
-        specty= np.fft.rfft(yh, axis=0) 
-        y_db = kalibrering(2, freq, specty, dBA_dict)
-        L = ekvivalentverdi2(sample_period, len(y_db), y_db)
-'''
+        dBA_num = dBA(freq, spectrum, dBA_dict)
+        plt.plot(freq, dBA_num)
+        plt.show()
+
+        
+        #yy, yh = butter_bandpass_filter(x, 200, 800, 31250, order=8)
+        #specty= np.fft.rfft(yh, axis=0) 
+        T = num_of_samples * sample_period
+        L = ekvivalentnivå_mv0(data, v0)
+        
+        #L_kalib = kalibrering(2, freq, spectrum, dBA_dict)
+        tid += 1
+        tid_vec.append(tid)
+
+        print(L)
+        #a = plt.stem(tid, L)
+        L_eq.append(L)
+    
+    plt.plot(tid_vec, L_eq)
+    plt.show()
+
+plott(2, liste_filer) 
+
 
 
 
 #Testing
+'''
 a = 0.02
 f0 = 600.0
 x = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))
@@ -170,3 +197,8 @@ plt.subplot(2, 1, 2)
 plt.plot(freq, y_db)
 
 plt.show()
+'''
+
+
+
+
