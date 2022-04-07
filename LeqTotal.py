b52f009e3ed83e694f997cfc20e7e944c9a078e5
ankelefs <anker@lefstad.net>
2022-04-07 12:30:54 +0200

    .

--- LeqTotal.py
+++ LeqTotal.py
@@ -15,9 +15,13 @@ import scipy
 from scipy import signal
 import scipy.signal as signal
 from scipy.signal import butter, lfilter, freqz
+import sys
 
 fs = 31250
 v0 = 0.002
+
+filnavn = str(sys.argv[1])                      ## Run: python LeqTotal.py 'filnavn.ext'
+pathToFile = "Opptaksfiler/" + filnavn
 
 # dBA_dict = {6.3: -85.4, 8: -77.6, 10: -70.4, 12.5: -63.6, 16: -56.4, 20: -50.4, 25: -44.8, 31.5: -39.5, 40: -34.5, 50: -30.3, 63: -26.2, 80: -22.4, 100: -19.1, 125: -16.2, 160: -13.2, 200: -10.8, 250: -8.7, 315: -6.6, 400: -4.8, 500: -3.2, 630: -1.9, 800: -0.8, 1000: 0.0, 1250: 0.6, 1600: 1.0, 2000: 1.2, 2500: 1.3, 3150: 1.2, 4000: 1.0, 5000: 0.6, 6300: -0.1, 8000: -1.1, 10000: -2.5, 12500: -4.3, 16000: -6.7, 20000: -9.3} #inneholder tabellen over generelle dBA verdier
 
@@ -58,7 +62,7 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     return y
 
 # Import data from bin file
-sample_period, data = raspi_import('420Hz.bin')
+sample_period, data = raspi_import(pathToFile)
 
 data = signal.detrend(data, axis=0) # removes DC component for each channel
 sample_period *= 1e-6  # change unit to micro seconds
