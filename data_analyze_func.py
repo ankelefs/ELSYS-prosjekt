eae4fac202c9860592a571c75bddeed724d87a38
Rikke00 <Rikke.m.nielsen@outlook.com>
2022-04-07 08:37:04 +0200

    lagt til filer

---
+++ data_analyze_func.py
@@ -0,0 +1,15 @@
+from cProfile import label
+from cmath import cos
+import dataclasses
+import math
+from turtle import xcor
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy import signal
+import scipy.signal as signal
+from scipy.signal import butter, lfilter, freqz
+import os
+from scipy.signal import butter, lfilter, freqz, filtfilt, sosfilt
+
+
