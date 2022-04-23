37c6d5c9d00788111e055d3a1ea780350dfa537d
KristianReigstad <krireig@gmail.com>
2022-04-23 18:51:08 +0200

    LagtTilMerKode

--- Kalibrering.py
+++ Kalibrering.py
@@ -2,6 +2,9 @@
 import numpy as np
 import data_analyze_func as daf
 import matplotlib.pyplot as plt
+import scipy
+from scipy import signal
+import scipy.signal as signal
 
 '''
 Denne brukes kun til kalibrering av mikrofonen. Brukes ikke til behandlig av data. Den nederste funksjonen skal brukes.
@@ -20,46 +23,62 @@ def finn_kalibrering(kalib_fil, målt_verdi):
     diff = np.argmax(spectrum) - målt_verdi
     return diff
 '''
-vv0 = 0.068 #testvariabel
+vv0 = 0.05586528 #Referansenivå
+T = 62500   #Antall samples i 2 sekund lydfil
 referansenivaa = 75   #referansenivå = målt_verdi_Leq = 75
 
 #kalib_fil = "Opptaksfiler\\Lydfiler-fra-kalibrering-på-lab\\Y2022-M04-D07-H12-M56-S14.bin" #for windows
 
-kalib_fil = "Opptaksfiler/Lydfiler-fra-kalibrering-på-lab/Y2022-M04-D07-H12-M56-S14.bin" #for mac
+kalib_fil = "Opptaksfiler/Lydfiler-fra-kalibrering-på-lab/Testing-på-grupperom-etter-lab/Y2022-M04-D07-H17-M24-S47.bin" #for mac
+
+## Ubruktebeholdere
+"""sentrertData = []"""
+# toSekDataVektor = []
 
 def finn_v0(kalib_fil, målt_verdi_Leq):
     sample_period, data = daf.raspi_import(kalib_fil)
     num_of_samples = data.shape[0]
-    print("###")
-    print(len(data))
-    print("v0:")
-    print(daf.v0)
-    print("Ekvivalentniva:")
-    ekvi = daf.ekvivalentniva_mv0(data, vv0)
-    print(ekvi)
-    
-    print(len(data))
+    data = signal.detrend(data, axis=0)
+
+    # print(len(data))
     # for x in data:
     #     print(x)
     
     # T = num_of_samples  * sample_period
-    T = 62500
+    
     sum_V = 0
-    sum_datavektor = 0
+    
     # for-løkke
     for i in range(0, T):
-        if(data[i] < 791):
-            temp = float(-790 + data[i]) 
-        else:
-            temp = float(data[i]-790)
-
-        sum_V += (temp)**2
-        print(temp)
-   
-    #Finn ut hvilken som gir riktig resultat for v0:    
-    # utregnet_v0 = np.sqrt(1/(T * 1/(10**(målt_verdi_Leq/20))) * sum_V) #(målt_verdi_Leq = 75)
+        
+        ##sum_V inneholder summen av samplingene kvadrert.
+        sum_V += (data[i])**2
+        
+        
+        # ####Sentrerer samplingene manuelt. Bruker i stedet signal.detrend()
+        # ##Sentrerer samplingene om 0 ved å trekke fra offset.
+        # if(data[i] < 771.038656):
+        #     temp = float(-771.038656 + data[i]) 
+        # else:
+        #     temp = float(data[i]-771.038656)
+        # # print(temp)
+        """sentrertData.append(data[i])"""
+        
     utregnet_v0 = np.sqrt(sum_V/(T*10**(målt_verdi_Leq/10)))
     return utregnet_v0
+        
+    
+    # ##Variabel som holder summen av målingene for å finne offset
+    #     toSekDataVektor.append(float(data[i]))
+    
+    # ##Bruker gjennomsnittet av to sekund sampling for å finne offset
+    # offset = sum(sentrertData)/len(sentrertData)    
+    # print("Offset:")
+    # print(offset)
+         
+    #Utregnet #(målt_verdi_Leq = 75)
+
+   
 
 referanseverdi = finn_v0(kalib_fil, referansenivaa)
 print("Utregnet v0:")
@@ -72,9 +91,13 @@ print("Ikke riktig?")
 # Generate time axis
     
 sample_period, data = daf.raspi_import(kalib_fil)
-
 num_of_samples = data.shape[0]  # returns shape of matrix
-t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
+data = signal.detrend(data, axis=0)
+
+t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples) #For å få 10 sekund byttes T ut med num_of_samples. T gjelder for 2 sekund.
+
+# for item in t:
+#     print(item)
 
 plt.subplot(2, 1, 1)
 plt.title("Time domain signal")
@@ -82,10 +105,16 @@ plt.xlabel("Time [us]")
 plt.ylabel("Voltage")
 plt.plot(t, data)
 
-#plt.show()
-plt.savefig('TesterInnholdIData')
+plt.show()
+
+# plt.savefig('TesterInnholdIData')
     
 ########SLUTT PLOTTING###############
+
+
+print("Ekvivalentniva:")
+ekvi = daf.ekvivalentniva_mv0(data, vv0)
+print(ekvi)
 
 
 # def finn_kalibrering(kalib_fil, målt_verdi):
