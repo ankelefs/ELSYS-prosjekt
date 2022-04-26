741ea6b983b359e0409f6ea0705942c5e7e4e7cb
ankelefs <anker@lefstad.net>
2022-04-26 15:03:59 +0200

    Demo

--- data_analyze_demo.py
+++ data_analyze_demo.py
@@ -20,6 +20,7 @@ mappe_midlertidig_plassering = '/Users/ankerlefstad/Desktop/sonuscaptura-demo/el
 
 # Ti minutter
 time_sleep = 60*10
+# time_sleep = 10
 # Fra kalibrering
 v0 = 0.00770143
 
@@ -28,6 +29,9 @@ v0 = 0.00770143
 def analysisRuntimeDuration(time_after_analysis, time_before_analysis):
     # Henter ut differansen i antall sekund fra time_before_analysis og time_after_analysis
     return int((time_after_analysis - time_before_analysis).total_seconds()) 
+
+def cleanup():
+    os.system(f'cd {mappe_opptaksfiler} && mv *.bin Midlertidig-plassering')
 
 
 ##### PROGRAMMET #####
@@ -38,11 +42,15 @@ if __name__ == '__main__':
 
     # Skal kjøres i uendelig tid
     while True: 
+        print('##### KJØRING #####')
+        
         # Synkronisering #2
         time_before_analysis = datetime.datetime.now()
         
         # Henter nye opptaksfiler:
-        # pull_and_remove.pullFromGit(elsysProsjektMappeServer)
+        print('>>> Henter fra GitHub ...')
+        pull_and_remove.pullFromGit(elsys_prosjekt)
+        print('==> Hentet fra GitHub')
         
         ##### Analyse av data #####
         # Vil produsere ETT plott med all informasjon som skal vises på hjemmesiden
@@ -62,44 +70,46 @@ if __name__ == '__main__':
                 t = np.linspace(start=0, stop=num_of_samples * sample_period, num=num_of_samples)
                 
                 # Analyser opptaksfil
+                print('>>> Analyserer opptaksfil ...')
                 tempLeq = data_analyze_func.ekvivalentniva_mv0(data, v0)
                 
                 # Henter tidspunkt fra filnavnet
                 filename_comp = filename.split('-')
         
         # Lag plott
+        print('>>> Lager plott ...')
         # Lydopptaket
-        plt.plot(t[100:(31250+100)*10], data[100:(31250+100)*10])
+        plt.plot(t[100:(31250+100)*10], data[100:(31250+100)*10], color="#BDD545")
         plt.xlim(0, 10)
         plt.xlabel('Tid [s]')
         plt.ylabel('Spenning [mV]')
-        plt.title('Utdrag på ti sekund fra en ti minutters støymåling')
+        plt.title('Utdrag fra støymålingen')
+        plt.tight_layout()
+        # plt.show()
+        # plt.savefig('HVOR?/fig.png')
+        print('==> Plott lagret')
         
-        # Tiden opptaket ble gjort
-        my_image = Image.open("white-background.png")
-
-        image_editable = ImageDraw.Draw(my_image)
-        image_editable.text((15,15), f'Tid: {filename_comp[4]}:{filename_comp[5]}', (237, 230, 211))
-
-        my_image.save("fig-info.png")
-        
-        # plt.text(0, 0, ')
-        # plt.text(10, 10, ) f'Mest fremtredende frekvens: {tempLeq}'
-        
-        # Frekvensspekteret
-        # Ingen plotting
-
-        # fig.tight_layout()
-        plt.show()
-        plt.savefig('fig.png')
-    
+        # Annen info
+        print('>>> Lager info ...')
+        with Image.open("white-background.png") as im:
+            draw_im = ImageDraw.Draw(im)
+            
+            my_font = ImageFont.truetype('/Users/ankerlefstad/Desktop/sonuscaptura-demo/elsys-prosjekt/Roboto/Roboto-Medium.ttf', 60)
+            
+            # Ikke endre whitespace. Dette er formatet
+            draw_im.text((50, 50), f'Ekvivalentnivå siste 5 minutter:   {tempLeq:.2f} dB\nNår målingen ble tatt:                    {filename_comp[3].strip("H")}:{filename_comp[4].strip("M")}:{filename_comp[5].strip("S").split(".")[0]}', fill = (66, 102, 122), font=my_font)
+            
+            # im.show()
+            # im.save('HVOR?/fig-info.png')
+            print('==> Info lagret')
             
         # Flytt ferdigbehandlede opptaksfiler til Midlertidig-plassering-mappen
-        # pull_and_remove.removeBinaryFiles(elsysProsjektMappeServer, elsysProsjektOpptaksfilerMappeServer)
+        cleanup()
+        print('==> Flyttet opptaksfilen')
         
         # Pause i ti minutter minus tiden databehandlingen tok
-        # time_after_analysis = datetime.datetime.now()
-        # pause_time = time_sleep - analysisRuntimeDuration(time_after_analysis, time_before_analysis)
-        # print(f'Sekunder til neste kjøring: {pause_time} s')
-        # time.sleep(pause_time)
+        time_after_analysis = datetime.datetime.now()
+        pause_time = time_sleep - analysisRuntimeDuration(time_after_analysis, time_before_analysis)
+        print(f'==> Sekunder til neste kjøring: {pause_time}')
+        time.sleep(pause_time)
         
